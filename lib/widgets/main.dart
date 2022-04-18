540342517a1f895b58762dedc08b0d2002133cbb
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 19:21:44 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -73,4 +73,4 @@ class MyApp extends StatelessWidget{
                     color: Colors.white),
               ),
               iconTheme: IconThemeData(
-                color: Color   
+                color: Colors   
