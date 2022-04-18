2b217ce23017015dfdfda0cfbb6626649f82e632
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 17:40:21 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget{
           value: Game(),
         ),
 
-        // It is lazily buil 
+        // It is lazily build 
             
                
             
