d490d85f06f102667b5d28bf03eed6d4a780d15f
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 16:27:12 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
  
  
 
