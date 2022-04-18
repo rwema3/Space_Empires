6565ddc95b13f38652032e2eae5d5e050450afa2
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 16:26:33 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -6,7 +6,6 @@ import 'package:sizer/sizer.dart';
 
 import './screens/character_selection_screen.dart';
 import './screens/game_end/game_lost.dart';
-import './screens/game_end/game_won.dart';
  
  
 
