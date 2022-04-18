957971e568a1744d8e5638fd34ec57f5d8f84970
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 18:53:48 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -59,7 +59,7 @@ class MyApp extends StatelessWidget{
             debugShowCheckedModeBanner: false,
             title: 'Space Empires',
             theme: ThemeData.dark().copyWith(
-              scaffoldBackgroundCol
+              scaffoldBackgroundColz
               
                
              
