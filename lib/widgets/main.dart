3d658c80eadc1c512e25489b03cd65a48809ca6b
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 15:43:57 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget{
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<Game>.value(
-          val 
+          va 
         
 
             
