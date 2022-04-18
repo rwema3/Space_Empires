f41b140311cc6f245788fd3973788cee6a291cca
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 09:41:40 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -130,5 +130,4 @@ class MyApp extends StatelessWidget{
                 final Planet _planet = args['planet'] as Planet;
                 final Player _attacker = args['attacker'] as Player;
                 return MaterialPageRoute(
-                  builder: (context) =>
    
