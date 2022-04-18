6e526bb58b10b87f334f4d8799801fff1eabd13c
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 15:44:46 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget{
     SystemChrome.setEnabledSystemUIOverlays([]);
     return MultiProvider(
       providers: [
-        ChangeNotifierProvide
+        ChangeNotifierProvid
            
         
 
