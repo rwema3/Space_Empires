954ea53a7516f589571ae104d995536fe64c9f65
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 18:35:53 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -54,7 +54,7 @@ class MyApp extends StatelessWidget{
         ),
       ],
       builder: (_, __) {
-        return Sizer(builder: (context, orientation, d 
+        return Sizer(builder: (context, orientation, de 
            
              
               
