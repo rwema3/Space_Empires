389bf568ade9bb53cec3cc24445b0de1e36e5fb6
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 19:05:35 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -64,7 +64,7 @@ class MyApp extends StatelessWidget{
               accentColor: Palette.deepBlue,
               textTheme: TextTheme(
                 headline4: TextStyle(fontSize: 28.sp, color: Colors.white),
-                bodyText 
+                bodyText2 
                  
 
           
