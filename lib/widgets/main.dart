2d41b849fdc67e1481aecc3acda1a75f28e8acee
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 17:46:38 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget{
         ),
 
         // It is lazily build, i.e unless someone requests it, it won't be created
-        // It is requested for first Time on gameSc 
+        // It is requested for first Time on gameScr 
                
             
            
