2b34a89bcf8bc98136534e90393e9368b659229e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 15:09:40 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget{
         // It is requested for first Time on gameScreen (as we navigate from characterSelectionScreen)
         // As soon as gameScreen is called create is called
         // But since we notify GameData too (in characterSelectionScreen)
-        // So update is calle
+        // So update is call
           
        
               
