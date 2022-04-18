dbff84fe25919192f23440c0839368540c9a2d3d
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 14:14:47 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -54,7 +54,7 @@ class MyApp extends StatelessWidget{
         ),
       ],
       builder: (_, __) {
-        return Sizer(builder: (co 
+        return Sizer(builder: (c 
            
              
               
