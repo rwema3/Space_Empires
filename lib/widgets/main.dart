60286aabfa07a40680835e665772ad7d8c89c638
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 18:18:48 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -49,7 +49,7 @@ class MyApp extends StatelessWidget{
             return game.currentPlayer;
           },
           create: (ctx) {
-            return n
+            return nu
           
         
       
