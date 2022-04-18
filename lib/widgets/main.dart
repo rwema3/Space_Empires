63d6314c5148be858626859397651ca8ee459d3e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-18 12:23:55 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -58,7 +58,7 @@ class MyApp extends StatelessWidget{
           return MaterialApp(
             debugShowCheckedModeBanner: false,
             title: 'Space Empires',
-            theme: 
+            theme 
               
               
                
