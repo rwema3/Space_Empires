af7b778c6d1d2e4bb768ae11c56bcc684fe4cc2f
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-23 22:06:47 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -62,8 +62,6 @@ class MyApp extends StatelessWidget{
               scaffoldBackgroundColor: Colors.black,
               primaryColor: Colors.indigo,
               accentColor: Palette.deepBlue,
-              textTheme: TextTheme(
-                headline4: TextStyle(fontSize: 28.sp, color: Colors.white),
               
                     TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w600),
                 unselectedLabelStyle:
