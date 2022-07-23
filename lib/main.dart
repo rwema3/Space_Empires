7590a1a13fe8cfa8bebf4ba6587cefbae5ee012c
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-23 22:06:36 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,6 @@ class MyApp extends StatelessWidget{
                 bodyText2: TextStyle(fontSize: 12.sp, color: Colors.white),
                 headline5: TextStyle(fontSize: 18.sp, color: Colors.white),
                 headline6: TextStyle(fontSize: 16.sp, color: Colors.white),
-                button: TextStyle(
               
                     TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w600),
                 unselectedLabelStyle:
