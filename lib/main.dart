22dbcf458348424940f30bc75c534e25a2a2efe9
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-23 22:06:16 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -76,8 +76,7 @@ class MyApp extends StatelessWidget{
                 color: Colors.white,
                 size: 18.sp,
               ),
-              tabBarTheme: TabBarTheme(
-                
+              
                     TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w600),
                 unselectedLabelStyle:
                     TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w600),
