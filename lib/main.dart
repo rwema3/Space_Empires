b61e31cb54934c3bcd580b98219606bf9df39526
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-26 15:10:17 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -72,6 +72,8 @@ class MyApp extends StatelessWidget{
                     fontWeight: FontWeight.bold,
                     color: Colors.white),
               ),
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsets>(
                       EdgeInsets.all(4.sp)),
                   backgroundColor:
                       MaterialStateProperty.all<Color>(Palette.deepBlue),
