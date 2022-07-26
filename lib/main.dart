191ec538da459dc2f63df912ff183c0df9beba48
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-26 15:10:00 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -72,6 +72,7 @@ class MyApp extends StatelessWidget{
                     fontWeight: FontWeight.bold,
                     color: Colors.white),
               ),
+                    TextStyle(fontSize: 12.sp, fontWeight: FontWeight.bold),
                   ),
                 ),
               ),
