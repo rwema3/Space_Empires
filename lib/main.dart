ef8d36683771738bebd1854d9cb1fc19aaf658d9
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-29 09:44:19 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,3 +33,20 @@ class MyApp extends StatelessWidget{
     // To make the Game fullscreen
     SystemChrome.setEnabledSystemUIOverlays([]);
     return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<Game>.value(
+          value: Game(),
+        ),
+
+        // It is lazily build, i.e unless someone requests it, it won't be created.
+        // It is requested for first Time on gameScreen (as we navigate from characterSelectionScreen)
+        // As soon as gameScreen is called create is called
+        // But since we notify or tell GameData too (in characterSelectionScreen)
+        // So update is called too
+        // finally the value obtained is provided in gameScreen
+        ChangeNotifierProxyProvider<Game, Player?>(
+          update: (_, game, __) {
+            return game.currentPlayer;
+          },
+          create: (ctx) {
+            return null;
