29df9f6c8d80e335695446b7201d160202c8e340
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-29 09:44:14 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
+
+import './screens/character_selection_screen.dart';
+import './screens/game_end/game_lost.dart';
+import './screens/game_end/game_won.dart';
+import './screens/game_screen.dart';
+import './screens/planet_screen.dart';
+import './screens/splash_screen.dart';
+import './screens/story/story_i.dart';
+import './screens/story/story_ii.dart';
+import './screens/welcome_screen.dart';
+import 'models/planet_model.dart';
+import 'screens/attack/attack_conclusion_screen.dart';
