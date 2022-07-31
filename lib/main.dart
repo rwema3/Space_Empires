cc1871d3b32cceb13fd6b99dba3481e63e6e3077
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-31 08:54:37 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -100,6 +100,18 @@ class MyApp extends StatelessWidget{
               SplashScreen.route: (ctx) => SplashScreen(),
               StoryScreenII.route: (ctx) => StoryScreenII(),
               StoryScreenIII.route: (ctx) => StoryScreenIII(),
+             
+            onGenerateRoute: (routeSettings) {
+              if (routeSettings.name == InfoScreen.route) {
+                return MaterialPageRoute(
+                    builder: (context) => InfoScreen(
+                        characterSelected: routeSettings.arguments! as bool));
+              } else if (routeSettings.name == PlanetScreen.route) {
+                final PlanetName _planetName =
+                    routeSettings.arguments! as PlanetName;
+                return MaterialPageRoute(
+                  builder: (context) => PlanetScreen(_planetName),
+                );
               } else if (routeSettings.name == StoryScreenI.route) {
                 final Orientation _orientation =
                     routeSettings.arguments! as Orientation;
