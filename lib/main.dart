199412086e9ad3b589edf5fed4c1d44bec4e4bca
Rwema <52289151+rwema3@users.noreply.github.com>
2022-08-02 17:32:03 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,35 +35,7 @@ class MyApp extends StatelessWidget{
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<Game>.value(
-          value: Game(),
-        ),
-
-        // It is lazily build, i.e unless someone requests it, it won't be created.
-        // It is requested for first Time on gameScreen (as we navigate from characterSelectionScreen)
-        // As soon as gameScreen is called create is called
-        // But since we notify or tell GameData too (in characterSelectionScreen)
-        // So update is called too
-        // finally the value obtained is provided in gameScreen
-        ChangeNotifierProxyProvider<Game, Player?>(
-          update: (_, game, __) {
-            return game.currentPlayer;
-          },
-          create: (ctx) {
-            return null;
-          },
-        ),
-      ],
-      builder: (_, __) {
-        return Sizer(builder: (context, orientation, deviceType) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Space Empires',
-            theme: ThemeData.dark().copyWith(
-              scaffoldBackgroundColor: Colors.black,
-              primaryColor: Colors.indigo,
-              accentColor: Palette.deepBlue,
-              textTheme: TextTheme(
-             
+          val
             },
           );
         });
