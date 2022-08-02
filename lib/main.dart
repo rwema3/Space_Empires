94a824cc900d029cacf9c1e21fd4ff1abdd258ce
Rwema <52289151+rwema3@users.noreply.github.com>
2022-08-02 17:31:41 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -124,16 +124,7 @@ class MyApp extends StatelessWidget{
                 return MaterialPageRoute(
                   builder: (context) => StoryScreenI(_orientation),
                 );
-              } else if (routeSettings.name == AttackScreen.route) {
-                final args = routeSettings.arguments! as Map;
-                final Planet _planet = args['planet'] as Planet;
-                final Player _attacker = args['attacker'] as Player;
-                return MaterialPageRoute(
-                  builder: (context) =>
-                      AttackScreen(attacker: _attacker, planet: _planet),
-                );
-              }
-              return null;
+             
             },
           );
         });
