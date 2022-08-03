853597070ae3aa64ff37bb6420326ea84c735b27
Rwema <52289151+rwema3@users.noreply.github.com>
2022-08-03 11:39:58 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -103,11 +103,7 @@ class MyApp extends StatelessWidget{
               WelcomeScreen.route: (ctx) => WelcomeScreen(),
               CharacterSelectionScreen.route: (ctx) =>
                   CharacterSelectionScreen(),
-              GameScreen.route: (ctx) => GameScreen(),
-              AttackConclusionScreen.route: (ctx) => AttackConclusionScreen(),
-              GameLostScreen.route: (ctx) => GameLostScreen(),
-              GameWonScreen.route: (ctx) => GameWonScreen(),
-            },
+           
             onGenerateRoute: (routeSettings) {
               if (routeSettings.name == InfoScreen.route) {
                 return MaterialPageRoute(
